a662b52049648a1759dbfd4ef7935fb5000a25b8
polic72 <>
2022-10-06 00:48:34 -0400

    Slight spacing differences in Hello World.

--- Hello World/gtk-test.c
+++ Hello World/gtk-test.c
@@ -14,7 +14,7 @@ static void activate(GtkApplication* app, gpointer user_data)
 
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window), "Window");
-	gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
+	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
 	button = gtk_button_new_with_label("Hello World");
 	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
@@ -31,9 +31,9 @@ int main(int argc, char** argv)
 
 	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
 
-	g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-	status = g_application_run(G_APPLICATION (app), argc, argv);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
 
 	g_object_unref(app);
 
